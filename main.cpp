43bfef7282003d1259df2302bbffcc3f577c2638
AbdTesting <akh_066@hotmail.com>
2015-02-04 12:37:43 -0500

    Added all files

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 int main()
 {
-printf("\nHello World,\nWelcome to my first C++ program in Ubuntu Linux\n\n");
+printf("\nHello World,\nWelcome to my first C++ program in Ubuntu Linux.\n\n");
 return(0);
+
 }
